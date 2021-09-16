a9520ecafd3efa11b75aa11436dcaae8aec7ea0c
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 15:11:44 -0300

    Criar classe cliente

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/Cliente.java
@@ -0,0 +1,55 @@
+package br.com.maquininha.cartao.maquininha.cartao.cliente;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Column(unique = true, nullable = false)
+    private String nome;
+    @Column(unique = true, nullable = false)
+    private String estabelecimento;
+    @Id
+    private String email;
+    @Column(nullable = false)
+    private String senha;
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(String estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
