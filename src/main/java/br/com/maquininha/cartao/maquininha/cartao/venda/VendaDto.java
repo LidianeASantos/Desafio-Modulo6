d1df7d319f0c24143e854ffd1cba0de3bc0192e6
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 16:57:12 -0300

    Criar classe venda dto

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaDto.java
@@ -0,0 +1,49 @@
+package br.com.maquininha.cartao.maquininha.cartao.venda;
+
+import javax.validation.constraints.NotBlank;
+
+public class VendaDto {
+
+    private int id;
+    @NotBlank
+    private double valor;
+    @NotBlank
+    private Opcao opcao;
+    @NotBlank
+    private int qtdParcela;
+
+    public VendaDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Opcao getOpcao() {
+        return opcao;
+    }
+
+    public void setOpcao(Opcao opcao) {
+        this.opcao = opcao;
+    }
+
+    public int getQtdParcela() {
+        return qtdParcela;
+    }
+
+    public void setQtdParcela(int qtdParcela) {
+        this.qtdParcela = qtdParcela;
+    }
+}
