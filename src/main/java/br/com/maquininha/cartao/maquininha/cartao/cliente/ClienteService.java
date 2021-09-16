9c0241c2078a16b151f803ea72ed08b69a183afe
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 16:50:10 -0300

    Refatorar classe cliente service

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/ClienteService.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.maquininha.cartao.maquininha.cartao.cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+
 @Service
 public class ClienteService {
 
@@ -10,8 +12,7 @@ public class ClienteService {
     private ClienteRepository clienteRepository;
 
     public Cliente cadastrarCliente(Cliente cliente){
-        clienteRepository.save( cliente );
-        
-        return cliente;
+       return clienteRepository.save( cliente );
+
     }
 }
