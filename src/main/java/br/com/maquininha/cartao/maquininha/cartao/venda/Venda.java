dd0ba09de38ef933c9247fc264ff7fab1468d1bc
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 15:02:05 -0300

    Criar classe venda

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/Venda.java
@@ -0,0 +1,53 @@
+package br.com.maquininha.cartao.maquininha.cartao.venda;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="vendas")
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private double valor;
+    private Opcao opcao;
+    private int qtdParcela;
+
+
+    public Venda() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Opcao getOpcao() {
+        return opcao;
+    }
+
+    public void setOpcao(Opcao opcao) {
+        this.opcao = opcao;
+    }
+
+    public int getQtdParcela() {
+        return qtdParcela;
+    }
+
+    public void setQtdParcela(int qtdParcela) {
+        this.qtdParcela = qtdParcela;
+    }
+
+   }
