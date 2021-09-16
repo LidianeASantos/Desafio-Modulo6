22050642a914d96bb9c66c78350f82c9cfba00d2
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 14:30:33 -0300

    Setup do Projeto

---
+++ src/test/java/br/com/maquininha/cartao/maquininha/cartao/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maquininha.cartao.maquininha.cartao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
