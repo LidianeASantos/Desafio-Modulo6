4df9321f2420db3a365af88d8b3f3de5e4228424
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 17:11:16 -0300

    Implementar cadastro de venda classe venda controller

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaController.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaController.java
@@ -1,8 +1,11 @@
 package br.com.maquininha.cartao.maquininha.cartao.venda;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/vendas")
@@ -10,4 +13,15 @@ public class VendaController {
 
     @Autowired
     private VendaService vendaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VendaDto cadastrarVenda(@RequestBody @Valid Venda venda){
+        Venda vendaModel = vendaService.cadastrarVenda( venda );
+
+        return modelMapper.map( vendaModel, VendaDto.class );
+    }
 }
