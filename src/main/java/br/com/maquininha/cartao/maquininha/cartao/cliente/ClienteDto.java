318848ed5d348221de8d285d445a2d6a067cb66d
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 16:09:08 -0300

    Criar classe cliente dto

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/ClienteDto.java
@@ -0,0 +1,53 @@
+package br.com.maquininha.cartao.maquininha.cartao.cliente;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class ClienteDto {
+
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String estabelecimento;
+    @Email
+    private String email;
+    @NotBlank
+    private String senha;
+
+    public ClienteDto() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(String estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
