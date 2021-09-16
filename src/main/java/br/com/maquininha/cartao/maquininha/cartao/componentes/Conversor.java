f7e2bbb40472e02eab53eb9004a30724fc46fbfa
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 16:47:24 -0300

    Incluir anotação na classe converssor model mapper

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/componentes/Conversor.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/componentes/Conversor.java
@@ -2,7 +2,9 @@ package br.com.maquininha.cartao.maquininha.cartao.componentes;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Conversor {
 
     @Bean
