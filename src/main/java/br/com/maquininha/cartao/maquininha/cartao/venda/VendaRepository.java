336f5eeac8d0ee11c9244b61cd44675aeb4f046d
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-16 15:36:39 -0300

    Criar classe cliente repository

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaRepository.java
@@ -0,0 +1,6 @@
+package br.com.maquininha.cartao.maquininha.cartao.venda;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VendaRepository extends CrudRepository<Venda, Integer> {
+}
