6ff9d4f0c307af53bc1af367d20d57f4adb87b53
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-09-20 14:55:36 -0300

    Mapeando metodo de buscar por id

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaController.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaController.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -19,9 +20,26 @@ public class VendaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public VendaDto cadastrarVenda(@RequestBody @Valid Venda venda){
-        Venda vendaModel = vendaService.cadastrarVenda( venda );
+    public VendaDto cadastrarVenda(@RequestBody @Valid Venda venda) {
+        Venda vendaModel = vendaService.cadastrarVenda(venda);
 
-        return modelMapper.map( vendaModel, VendaDto.class );
+        return modelMapper.map(vendaModel, VendaDto.class);
+    }
+
+    @GetMapping("/{buscarVendas}")
+    public Venda pesquisarVendaPorId(@PathVariable(name = "buscarVendas") int id) {
+        Venda venda;
+        try {
+            venda = vendaService.buscarVendaPorId(id);
+            return venda;
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarPeloID(@PathVariable int id){
+        vendaService.deletarVenda(id);
     }
 }
