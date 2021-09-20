9fb7051a3c1bdcf473379cf236d8434ae6d890b9
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-20 13:59:02 -0300

    Cadastrar método de busca por opcao na classe venda repository

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaRepository.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaRepository.java
@@ -2,5 +2,9 @@ package br.com.maquininha.cartao.maquininha.cartao.venda;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VendaRepository extends CrudRepository<Venda, Integer> {
+
+    List<Venda> findByVendaOpcaoContais(int debito);
 }
