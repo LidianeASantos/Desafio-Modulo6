f2b1d276374cbc6a5e175cde5c4faeeb3e02fb22
Robson <falecomrobsonsoares@gmail.com>
2021-09-21 14:21:29 -0300

    validação

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/exceptions/Erro.java
@@ -0,0 +1,17 @@
+package br.com.maquininha.cartao.maquininha.cartao.exceptions;
+
+public class Erro {
+    private String mensagem;
+
+    public Erro(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
