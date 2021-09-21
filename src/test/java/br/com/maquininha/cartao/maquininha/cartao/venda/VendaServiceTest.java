b7ac73c3fd83be526000aa89096fbbaf73dcfd1a
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-21 14:50:19 -0300

    Criar teste caminho negativo para busca de venda por id

--- src/test/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaServiceTest.java
+++ src/test/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaServiceTest.java
@@ -3,6 +3,7 @@ package br.com.maquininha.cartao.maquininha.cartao.venda;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.modelmapper.internal.bytebuddy.dynamic.DynamicType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,12 +28,22 @@ public class VendaServiceTest {
     }
 
     @Test
-    public void buscarVendaPorIdCaminhoPositivo(){
+    public void testarBuscarVendaPorIdCaminhoPositivo(){
         Venda venda = new Venda();
         Optional<Venda> vendaOptional = Optional.of(venda);
         Mockito.when(vendaRepository.findById( Mockito.anyInt())).thenReturn(vendaOptional);
 
         Assertions.assertEquals( venda, vendaService.buscarVendaPorId(1));
+    }
+
+    @Test
+    public void testarBuscarVendaPorIdCaminhoNegativo(){
+        Optional<Venda> vendaOptional = Optional.empty();
+        Mockito.when( vendaRepository.findById(Mockito.anyInt())).thenReturn(vendaOptional);
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class,
+                () -> {vendaService.buscarVendaPorId(1);});
+        Assertions.assertTrue( exception.getMessage().equals("Venda não localizada"));
     }
 }
 
