ce1ebf865a4a7a5cd66b1e846cbaa4520428647a
Robson <falecomrobsonsoares@gmail.com>
2021-09-21 15:18:47 -0300

    adicionar validação na classe venda dto

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaDto.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/venda/VendaDto.java
@@ -7,9 +7,9 @@ import javax.validation.constraints.NotBlank;
 public class VendaDto {
 
     private int id;
-    @NotBlank
+    @NotBlank(message = "{validacao.valor.obr}")
     private double valor;
-    @NotBlank
+    @NotBlank(message = "{validacao.opcao.obr}")
     private Opcao opcao;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private int qtdParcela;
