a95fa5ae11643c60d3ce9b2e62d3d3333e0d93be
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-21 09:31:18 -0300

    Excluir funcionalidades desnecessárias

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/resumovenda/ResumoVendaRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.maquininha.cartao.maquininha.cartao.resumovenda;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ResumoVendaRepository extends CrudRepository<ResumoVenda, Integer> {
-}
