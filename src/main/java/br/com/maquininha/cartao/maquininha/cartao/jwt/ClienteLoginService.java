1ba5ca00e140eace48dff347f3d4c18d6bdb3663
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-22 17:49:38 -0300

    Criar método de verificação de cliente/Usuário

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/jwt/ClienteLoginService.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/jwt/ClienteLoginService.java
@@ -1,9 +1,23 @@
 package br.com.maquininha.cartao.maquininha.cartao.jwt;
 
+import br.com.maquininha.cartao.maquininha.cartao.cliente.Cliente;
 import br.com.maquininha.cartao.maquininha.cartao.cliente.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public class ClienteLoginService {
     @Autowired
     private ClienteRepository clienteRepository;
+
+    public UserDetails loadUserByUserName(String username) throws UsernameNotFoundException{
+        Optional<Cliente> clienteOptional = clienteRepository.findByEmail(username);
+
+        clienteOptional.orElseThrow(() -> new UsernameNotFoundException("Cliente não encontrado!"));
+
+        Cliente cliente = clienteOptional.get();
+        return new ClienteLogin( cliente.getId(), cliente.getEmail(), cliente.getSenha() );
+    }
 }
