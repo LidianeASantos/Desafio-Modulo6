a97ffaf4ddfc23f1b41a25a138f5ba20120fd8ec
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-22 17:29:08 -0300

    Criar classe cliente login para autenticação do cliente

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/jwt/ClienteLogin.java
@@ -0,0 +1,77 @@
+package br.com.maquininha.cartao.maquininha.cartao.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class ClienteLogin implements UserDetails {
+    private int id;
+    private String email;
+    private String senha;
+
+    public ClienteLogin(int id, String email, String senha) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
