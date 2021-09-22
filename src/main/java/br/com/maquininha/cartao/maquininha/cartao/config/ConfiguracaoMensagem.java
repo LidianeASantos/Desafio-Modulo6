8c7cd3e6c7c3e828fa9fe8a78c45ee9e96ba4fe6
Robson <falecomrobsonsoares@gmail.com>
2021-09-21 23:06:10 -0300

    Resolver conflitos

---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/config/ConfiguracaoMensagem.java
@@ -0,0 +1,28 @@
+package br.com.maquininha.cartao.maquininha.cartao.config;
+
+        import org.springframework.context.MessageSource;
+        import org.springframework.context.annotation.Bean;
+        import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+        import org.springframework.stereotype.Component;
+        import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Component
+public class ConfiguracaoMensagem {
+
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource fonteDaMensagem = new ReloadableResourceBundleMessageSource();
+        fonteDaMensagem.setBasename("classpath:validacao");
+        fonteDaMensagem.setDefaultEncoding("UTF-8");
+        return fonteDaMensagem;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
+{
+}
---
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/exceptions/MensagemDeErro.java
@@ -0,0 +1,29 @@
+package br.com.maquininha.cartao.maquininha.cartao.exceptions;
+
+import java.util.List;
+
+public class MensagemDeErro {
+    private int statuscode;
+    private List<Erro> erros;
+
+    public MensagemDeErro(int statuscode, List<Erro> erros) {
+        this.statuscode = statuscode;
+        this.erros = erros;
+    }
+
+    public int getStatuscode() {
+        return statuscode;
+    }
+
+    public void setStatuscode(int statuscode) {
+        this.statuscode = statuscode;
+    }
+
+    public List<br.com.maquininha.cartao.maquininha.cartao.exceptions.Erro> getErros() {
+        return erros;
+    }
+
+    public void setErros(List<br.com.maquininha.cartao.maquininha.cartao.exceptions.Erro> erros) {
+        this.erros = erros;
+    }
+}
