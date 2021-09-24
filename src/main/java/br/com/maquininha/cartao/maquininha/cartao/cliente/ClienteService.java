7dcb335ce591c5f4b7abcef06189dab16f47d76a
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-24 09:32:29 -0300

    Criar método de busca de cliente por id

--- src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/ClienteService.java
+++ src/main/java/br/com/maquininha/cartao/maquininha/cartao/cliente/ClienteService.java
@@ -1,10 +1,12 @@
 package br.com.maquininha.cartao.maquininha.cartao.cliente;
 
+import br.com.maquininha.cartao.maquininha.cartao.exceptions.ClienteNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -18,6 +20,16 @@ public class ClienteService {
         String ecode = bCryptPasswordEncoder.encode((cliente.getSenha()));
         cliente.setSenha(ecode);
        return clienteRepository.save( cliente );
+
+    }
+
+    public Cliente pesquisarClientePorId(int id) throws ClienteNaoEncontradoException {
+        Optional<Cliente>clienteOptional = clienteRepository.findById( id );
+
+        if(clienteOptional.isPresent()){
+            return clienteOptional.get();
+        }
+        throw new ClienteNaoEncontradoException("Cliente não encontrado!");
 
     }
 }
